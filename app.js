67b83953027c1b73f1d5682115e78b41c861f0dd
Rodrigo Morgado <rodrigo@octopull.us>
2018-11-21 18:26:24 -0300

    working in geocode and requesting lat and long from an external api

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode.js');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var address = argv.address;
+const BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address?key=KRWZ4isO4JaY32CSVsEa3rL6oVkrcQah&location=';
+
+// We have to encode the string to remove spaces
 
 request({
-    url: 'http://www.mapquestapi.com/geocoding/v1/address?key=KRWZ4isO4JaY32CSVsEa3rL6oVkrcQah&location=1301%20lombard%20street%20philadelphia',
+    url: BASE_URL + encodeURIComponent(address),
     json: true
-}, (err, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+}, (error, response, body) => {
+    if (error){
+        console.log('Unable to connect to API');
+    // This errors are created using the response of google maps api
+    } else if (body.status === 'ZERO_RESULTS'){
+        console.log('Unable to find that location');
+    } else {
+        console.log(`Address: ${body.results[0].providedLocation.location}`);
+        console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+        console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+    }
+    
 });
