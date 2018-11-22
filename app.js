0723b220a9735affabbd7b6679853d4ec54cc3f5
Rodrigo Morgado <rodrigo@octopull.us>
2018-11-22 17:38:39 -0300

    Implementing the API to get the weather from an external server

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const request = require('request');
 
 const geocode = require('./geocode/geocode');
 
@@ -16,4 +17,22 @@ const argv = yargs
     .argv;
 
 var address = argv.address;
-console.log(geocode.getLocation(address));
+result = geocode.getLocation(address, (errorMessage, results) => {
+    if (errorMessage){
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2))
+        return JSON.stringify(results, undefined, 2);
+    }
+});
+
+request({
+    url: "https://api.darksky.net/forecast/d921247c4819bf682c80f1e191e9e12e/34.196973,-90.605302",
+    json: true
+}, (error, response, body) => {
+    if (!error && response.statusCode === 200){
+        console.log(body.currently.temperature);
+    } else {
+        console.log('Unable to fetch the weather');
+    }
+});
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -4,27 +4,29 @@ const BASE_URL = 'http://www.mapquestapi.com/geocoding/v1/address?key=KRWZ4isO4J
 
 // We have to encode the string to remove spaces
 
-var getLocation = (address) => {
+var getLocation = (address, callback) => {
 
     request({
         url: BASE_URL + encodeURIComponent(address),
         json: true
     }, (error, response, body) => {
         if (error){
-            console.log('Unable to connect to API');
+            callback('Unable to connect to API');
         // This errors are created using the response of google maps api
         } else if (body.status === 'ZERO_RESULTS'){
-            console.log('Unable to find that location');
+            callback('Unable to find that location')
         } else {
-            console.log(`Address: ${body.results[0].providedLocation.location}`);
-            console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-            console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+            callback(undefined, {
+                address: body.results[0].providedLocation.location,
+                latitude: body.results[0].locations[0].latLng.lat,
+                longitude: body.results[0].locations[0].latLng.lng
+            });
         }
-        
     });
-
 }
 
 module.exports = {
     getLocation
 }
+
+// https://api.darksky.net/forecast/d921247c4819bf682c80f1e191e9e12e/34.196973,-90.605302
